bf9d13879671b6cb0f7605f049aaca38383796f8
Hong Jae Baek <94942842+yk170901@users.noreply.github.com>
2023-12-06 14:24:31 +0900

    [Add] 회원가입 UI 제작 완료

--- Assets/3.Script/KYS/MainMenu/MainMenu_KYS.cs
+++ Assets/3.Script/KYS/MainMenu/MainMenu_KYS.cs
@@ -8,4 +8,37 @@ using UnityEngine;
 class MainMenu_KYS : MonoBehaviour
 {
     // test dbaccessmanager
+
+    // 로그인 버튼
+    public void Btn_Login()
+    {
+
+    }
+
+    private void ValidateLogin()
+    {
+
+    }
+
+    // 회원가입 버튼
+    public void Btn_SignUp()
+    {
+
+    }
+
+    private void ShowSignUpPanel()
+    {
+
+    }
+
+    // 회원가입 창의 제출 버튼
+    public void Btn_Submit()
+    {
+
+    }
+
+    private void ValidateSignUp()
+    {
+
+    }
 }
