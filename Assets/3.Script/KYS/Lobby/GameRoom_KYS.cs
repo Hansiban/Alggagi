10c10fd7dd7a59bfbcd7bcf1cb8af169969626d5
Hong Jae Baek <94942842+yk170901@users.noreply.github.com>
2023-12-07 17:58:50 +0900

    [Add] Lobby 진행 중 - 피씨방 가기 전 커밋

---
+++ Assets/3.Script/KYS/Lobby/GameRoom_KYS.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class GameRoom_KYS
+{
+    public string Name { get; private set; }
+
+    public bool IsWaiting = true;
+
+
+    // 게임 룸 데이터 모델을 가지고 씬을 만드나? 각 유저 방 별로 씬을 만들어야 하나?
+}
--- Assets/3.Script/KYS/Lobby/LobbyManager_KYS.cs
+++ Assets/3.Script/KYS/Lobby/LobbyManager_KYS.cs
@@ -1,9 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class LobbyManager_KYS : MonoBehaviour
 {
+    [SerializeField] private TMP_InputField _gameRoomNameInputField;
+
+    private List<GameRoom_KYS> _gameRooms;
+
+    private void Awake()
+    {
+        _gameRooms = new List<GameRoom_KYS>();
+    }
 
 
+    public void Btn_CreateGameRoom()
+    {
+        // _gameRoomNameInputField.text;
+        // _gameRooms.Add();
+    }
+
+    private void AddGameRoom()
+    {
+
+    }
 }
