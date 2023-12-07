4e08d91c6a4bb4ef3bc0e62670f79859060169fa
laurel3394 <b4comes@gmail.com>
2023-12-07 13:55:02 +0900

    [feat] 저장 불러오기?
    test code

---
+++ Assets/4.Sprite/JGD/PlayerData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class PlayerData : MonoBehaviour
+{
+    public string id;
+    public string password;
+    public string nickname;
+    public int level;
+    public int experience;
+    public int win;
+    public int lose;
+    public int draw;
+    public GameObject profile;
+}
--- Assets/4.Sprite/JGD/UserData_Save.cs
+++ Assets/4.Sprite/JGD/UserData_Save.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class UserData_Save : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public PlayerData playerData;
 
-    // Update is called once per frame
-    void Update()
+
+
+    //저장?
+    void SavePlayerDataJson()
     {
-        
+        string jsonData = JsonUtility.ToJson(playerData, true);
+
+        string path = Path.Combine(Application.dataPath, "playerData.Json");
+
+        File.WriteAllText(path, jsonData);
+    }
+    //불러오기?
+    private void LoadPlayerDataToJson()
+    {
+        string path = Path.Combine(Application.dataPath, "playerData.Json");
+        string JsonData = File.ReadAllText(path);
+        playerData = JsonUtility.FromJson<PlayerData>(JsonData);
     }
 }
