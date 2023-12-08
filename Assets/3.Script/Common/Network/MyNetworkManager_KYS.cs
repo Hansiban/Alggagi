1d2d35399263f79400e415edb3303e4145755d80
Hong Jae Baek <94942842+yk170901@users.noreply.github.com>
2023-12-08 09:27:57 +0900

    [Add] 머지 전 커밋 - 별 거 없음

--- Assets/3.Script/Common/Network/MyNetworkManager_KYS.cs
+++ Assets/3.Script/Common/Network/MyNetworkManager_KYS.cs
@@ -5,39 +5,46 @@ using UnityEngine;
 
 public class MyNetworkManager_KYS : NetworkManager
 {
+    #region
     //public static new MyNetworkManager_KYS singleton { get; private set; }
 
-    public List<NetworkConnectionToClient> list;
+    //public List<NetworkConnectionToClient> list;
 
-    public override void Awake()
-    {
-        base.Awake();
-        //singleton = this;
+    //public override void Awake()
+    //{
+    //    base.Awake();
+    //    //singleton = this;
 
-        list = new List<NetworkConnectionToClient>();
-    }
+    //    list = new List<NetworkConnectionToClient>();
+    //}
 
-    public override void OnServerConnect(NetworkConnectionToClient conn)
-    {
-        base.OnServerConnect(conn);
+    //public override void OnServerConnect(NetworkConnectionToClient conn)
+    //{
+    //    base.OnServerConnect(conn);
 
-        list.Add(conn);
+    //    list.Add(conn);
 
-        Debug.Log(conn.address + " 접속하였습니다.");
-    }
+    //    Debug.Log(conn.address + " 접속하였습니다.");
+    //}
 
-    public override void OnServerDisconnect(NetworkConnectionToClient conn)
-    {
-        base.OnServerDisconnect(conn);
+    //public override void OnServerDisconnect(NetworkConnectionToClient conn)
+    //{
+    //    base.OnServerDisconnect(conn);
 
-        list.Remove(conn);
+    //    list.Remove(conn);
 
-        Debug.Log(conn.address + " 접속 끊겼습니다.");
-    }
+    //    Debug.Log(conn.address + " 접속 끊겼습니다.");
+    //}
 
-    // Client 쪽에서 불리는 메소드라캄
-    public override void OnClientDisconnect()
-    {
-        base.OnClientDisconnect();
-    }
+    //// Client 쪽에서 불리는 메소드라캄
+    //public override void OnClientDisconnect()
+    //{
+    //    base.OnClientDisconnect();
+    //}
+    #endregion
+
+    Dictionary<string, NetworkRoomManager> _networkRoomManagers; // key = host id
+
+
+
 }
