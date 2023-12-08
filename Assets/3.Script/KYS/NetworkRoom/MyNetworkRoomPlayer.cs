cf07df57d5abe75b7f11487fbbf1f1c8ab099ed9
Hong Jae Baek <94942842+yk170901@users.noreply.github.com>
2023-12-08 15:54:49 +0900

    [Add] MyNetworkRoomManager와 MyNetworkRoomPlayer를 통해 대기실 및 게임씬에서 프로필 정보 받아 띄울 수 있도록 셋업

--- Assets/3.Script/KYS/NetworkRoom/MyNetworkRoomPlayer.cs
+++ Assets/3.Script/KYS/NetworkRoom/MyNetworkRoomPlayer.cs
@@ -9,4 +9,19 @@ public class MyNetworkRoomPlayer : NetworkRoomPlayer
     // 필요할지도 안 필요할지도
     [SerializeField] private GameObject _profilePrefab;
 
+    public UserDataModel_KYS UserData { get; private set; }
+
+    private void Awake()
+    {
+        Debug.Log("MyNetworkRoomPlayer AWAKE");
+
+        UserData = DbAccessManager_KYS.Instance.UserData;
+    }
+
+    public override void IndexChanged(int oldIndex, int newIndex)
+    {
+        base.IndexChanged(oldIndex, newIndex);
+
+        Debug.Log($"INDEX CHANGED from {oldIndex} to {newIndex}");
+    }
 }
