5e8765667cf1ea3e5c84d9e28a63b406018282d9
Hong Jae Baek <94942842+yk170901@users.noreply.github.com>
2023-12-11 10:09:03 +0900

    [Add] 게임 시작 버튼 클릭시 바로 자동으로 클라로 서버에 붙음

--- Assets/2.Model/Prefabs/_Test/OfflineRoomToWaitingRoomhandler.cs
+++ Assets/2.Model/Prefabs/_Test/OfflineRoomToWaitingRoomhandler.cs
@@ -14,16 +14,6 @@ public class OfflineRoomToWaitingRoomhandler : MonoBehaviour
     public IEnumerator LoadScene1SecLaterTest(string sceneName)
     {
         yield return new WaitForSeconds(1);
-
         SceneManager.LoadScene(sceneName);
-
-        MyNetworkRoomManager.singleton.StartClient();
-
-        Debug.Log("networkAddress before" + MyNetworkRoomManager.singleton.networkAddress);
-        Debug.Log("transport before" + MyNetworkRoomManager.singleton.transport);
-        MyNetworkRoomManager.singleton.networkAddress = "172.30.1.32";
-        //MyNetworkRoomManager.singleton.transport = new KcpTransport() { port = 7777 };
-
-        // 172.30.1.32에 연결해서 Client로 들어가기
     }
 }
--- Assets/3.Script/KYS/WaitingRoom/WaitingRoomManager.cs
+++ Assets/3.Script/KYS/WaitingRoom/WaitingRoomManager.cs
@@ -7,4 +7,19 @@ using UnityEngine.SceneManagement;
 public class WaitingRoomManager : MonoBehaviour
 {
     public void Btn_TempGoToMainMenu(string sceneName) => SceneManager.LoadScene(sceneName);
+
+    private MyNetworkRoomManager manager;
+
+    private void Start()
+    {
+        manager = FindObjectOfType<MyNetworkRoomManager>();
+
+        Debug.Log(SceneManager.GetActiveScene().name + "이 클라 쪽에서 로드됐습니다");
+
+        Debug.Log("networkAddress before" + manager.networkAddress);
+        manager.networkAddress = "172.30.1.32";
+        Debug.Log("networkAddress after" + manager.networkAddress);
+        //MyNetworkRoomManager.singleton.transport = new KcpTransport() { port = 7777 };
+        manager.StartClient();
+    }
 }
