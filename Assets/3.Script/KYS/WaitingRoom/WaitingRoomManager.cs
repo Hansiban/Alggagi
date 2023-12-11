6299a18280debd02d90707e4fe9e20dee2165179
yk170901 <yk170901@gmail.com>
2023-12-11 10:23:19 +0900

    서버 - [Add] 빌드세팅 수정 & 서버일 경우 WaitingRoom 넘어갔을 시 StartClient 안 하게 함

--- Assets/3.Script/KYS/WaitingRoom/WaitingRoomManager.cs
+++ Assets/3.Script/KYS/WaitingRoom/WaitingRoomManager.cs
@@ -1,6 +1,9 @@
+using LitJson;
 using Mirror;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,8 +13,12 @@ public class WaitingRoomManager : MonoBehaviour
 
     private MyNetworkRoomManager manager;
 
+    private Type type;
+
     private void Start()
     {
+        type = License_type();
+
         manager = FindObjectOfType<MyNetworkRoomManager>();
 
         Debug.Log(SceneManager.GetActiveScene().name + "이 클라 쪽에서 로드됐습니다");
@@ -19,7 +26,31 @@ public class WaitingRoomManager : MonoBehaviour
         Debug.Log("networkAddress before" + manager.networkAddress);
         manager.networkAddress = "172.30.1.32";
         Debug.Log("networkAddress after" + manager.networkAddress);
-        //MyNetworkRoomManager.singleton.transport = new KcpTransport() { port = 7777 };
-        manager.StartClient();
+
+        if (type.Equals(Type.Client))
+            manager.StartClient();
     }
+
+
+    private Type License_type()
+    {
+        try
+        {
+            string Json_string = File.ReadAllText(Application.dataPath + "/License.json");
+            JsonData itemdata = JsonMapper.ToObject(Json_string);
+
+            string string_type = itemdata[0]["Lisence"].ToString();
+            manager.networkAddress = itemdata[0]["Server_IP"].ToString();
+            type = (Type)Enum.Parse(typeof(Type), string_type);
+
+            return type;
+
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+            return Type.Empty;
+        }
+    }
+
 }
