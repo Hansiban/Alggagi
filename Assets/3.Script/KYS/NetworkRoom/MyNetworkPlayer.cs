2a6948cd7c62e02b131c1b9b090545bb2b1fdf1a
Hong Jae Baek <94942842+yk170901@users.noreply.github.com>
2023-12-11 09:18:39 +0900

    [Add] 메인 게임 씬 진입 시 Emoji Canvas 소환하고 자신과 상대의 프로필 정보 넣어줌

--- Assets/3.Script/KYS/NetworkRoom/MyNetworkPlayer.cs
+++ Assets/3.Script/KYS/NetworkRoom/MyNetworkPlayer.cs
@@ -2,11 +2,14 @@ using Mirror;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 
 // Network Room Manager를 사용할 때에 Player Prefab에 부여될 스크립트
 public class MyNetworkPlayer : NetworkBehaviour
 {
+    [SerializeField] private Canvas _emojiCanvasPrefab; // 유저 프로필도 담고 있으나 변수명 신경 쓸 상황 아님
+
     void Start()
     {
         if (!isLocalPlayer) return;
@@ -36,6 +39,7 @@ public class MyNetworkPlayer : NetworkBehaviour
         TargetGetOpponentData(oppData.Id, oppData.Pwd, oppData.Nick, oppData.Lvl, oppData.Exp, oppData.Win, oppData.Lose, oppData.Draw);
     }
 
+    // 하드코딩
     [TargetRpc]
     private void TargetGetOpponentData(string id, string pwd, string nick, int lvl, int exp, int win, int lose, int draw)
     {
@@ -44,13 +48,36 @@ public class MyNetworkPlayer : NetworkBehaviour
         Debug.Log("oppoData : " + oppData.ToString());
         Debug.Log("myData : " + GameManager.Instance.LocalUserData.ToString());
 
+        Debug.Log("해당 씬에서의 캔버스 개수 = "+FindObjectsOfType<Canvas>().Length);
+        Debug.Log("첫 번째 캔버스 이름 = "+FindObjectOfType<Canvas>().name);
+
         // 정보 채우기
-    }
+        Canvas canvas = Instantiate(_emojiCanvasPrefab);
+
+        PlayerProfile[] playerProfiles = canvas.GetComponentsInChildren<PlayerProfile>();
+        playerProfiles[0].Init(nick, lvl); // opp
+        playerProfiles[1].Init(GameManager.Instance.LocalUserData.Nick, GameManager.Instance.LocalUserData.Lvl); // my
 
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("playerProfiles 개수 = " + playerProfiles.Length);
+
+
+        // record containers
+        GameObject[] recordContainers = GameObject.FindGameObjectsWithTag("Container");
+        Debug.Log("recordContainers 개수 = " + recordContainers.Length);
+
+        //TMP_Text[] oppoRecordTxts = recordContainers[0].GetComponents<TMP_Text>(); // opp
+
+        List<TMP_Text> oppoRecordTxts = GameObject.FindGameObjectsWithTag("RecordText").Where(x => x.transform.parent == recordContainers[0].transform).Select(x => x.GetComponent<TMP_Text>()).ToList();
+        Debug.Log("oppoRecordTxts 개수 = " + oppoRecordTxts.Count);
+        oppoRecordTxts[0].text = win.ToString(); // oppo win
+        oppoRecordTxts[1].text = draw.ToString(); // oppo draw
+        oppoRecordTxts[2].text = lose.ToString(); // oppo lose
+
+        List<TMP_Text> myRecordTxts = GameObject.FindGameObjectsWithTag("RecordText").Where(x => x.transform.parent == recordContainers[1].transform).Select(x => x.GetComponent<TMP_Text>()).ToList();
+        Debug.Log("myRecordTxts 개수 = " + myRecordTxts.Count);
+        myRecordTxts[0].text = win.ToString(); // my win
+        myRecordTxts[1].text = draw.ToString(); // my draw
+        myRecordTxts[2].text = lose.ToString(); // my lose
     }
 }
